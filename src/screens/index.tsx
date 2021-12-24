2f11b5d512ae298fe63bf15fc46789e441aa6915
Pushpendra Upadhyay <pushpendraupadhyay6452@gmail.com>
2021-12-25 00:04:08 +0530

    Feat(SignIn): Setup signin screen

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,14 +1,31 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
-import { Button } from "@mui/material";
+import { Box, Button, createTheme, PaletteMode } from "@mui/material";
 import { Close } from "@mui/icons-material";
 import { SnackbarKey, SnackbarProvider } from "notistack";
 // import PrivateRoute from "../components/PrivateRoute";
 import SignIn from "./SignIn";
-import theme from "../theme";
+import { getThemeDesign } from "../theme";
+import ThemeSwitch from "../components/ThemeSwtich";
 
 const Screens = () => {
+  const [mode, setMode] = React.useState<PaletteMode>("light");
+  const colorMode = React.useMemo(
+    () => ({
+      // The dark mode switch would invoke this method
+      toggleColorMode: () => {
+        setMode((prevMode: PaletteMode) =>
+          prevMode === "light" ? "dark" : "light"
+        );
+      }
+    }),
+    []
+  );
+
+  // Update the theme only if the mode changes
+  const theme = React.useMemo(() => createTheme(getThemeDesign(mode)), [mode]);
+
   const notistackRef: React.RefObject<SnackbarProvider> = React.createRef();
 
   const onClickDismiss = (key: SnackbarKey) => () => {
@@ -27,6 +44,20 @@ const Screens = () => {
         hideIconVariant
         maxSnack={3}
       >
+        <Box
+          style={{
+            position: "static",
+            right: 10,
+            top: 10
+          }}
+        >
+          <ThemeSwitch
+            sx={{ m: 1 }}
+            defaultChecked
+            checked={mode === "dark"}
+            onChange={colorMode.toggleColorMode}
+          />
+        </Box>
         <BrowserRouter>
           <Switch>
             <Route path="/" children={SignIn} />
--- src/theme.ts
+++ src/theme.ts
@@ -1,10 +1,33 @@
-import { createTheme } from "@mui/material/styles";
-import { orange } from "@mui/material/colors";
+import { amber, deepOrange, grey } from "@mui/material/colors";
+import { PaletteMode } from "@mui/material";
 
-const theme = createTheme({
-  status: {
-    danger: orange[500]
+const getThemeDesign = (mode: PaletteMode) => ({
+  palette: {
+    mode
+    //   ...(mode === "light"
+    //     ? {
+    //         // palette values for light mode
+    //         primary: amber,
+    //         divider: amber[200],
+    //         text: {
+    //           primary: grey[900],
+    //           secondary: grey[800]
+    //         }
+    //       }
+    //     : {
+    //         // palette values for dark mode
+    //         primary: deepOrange,
+    //         divider: deepOrange[700],
+    //         background: {
+    //           default: deepOrange[900],
+    //           paper: deepOrange[900]
+    //         },
+    //         text: {
+    //           primary: "#fff",
+    //           secondary: grey[500]
+    //         }
+    //       })
   }
 });
 
-export default theme;
+export { getThemeDesign };
