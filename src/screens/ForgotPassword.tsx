1cdfc1741bc9a8880db8d24ba9776436170aa1c0
Pushpendra Upadhyay <pushpendra@kutuki.in>
2022-01-15 18:01:47 +0530

    Fixed console.log comment

--- src/screens/ForgotPassword.tsx
+++ src/screens/ForgotPassword.tsx
@@ -16,11 +16,11 @@ const ForgotPassword = () => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     // eslint-disable-next-line no-console
-    // console.log({
+    /* console.log({
       currentPassword: data.get("currentPassword"),
       password: data.get("password"),
       confirmPassword: data.get("confirmPassword")
-    });
+    }); */
   };
 
   return (
